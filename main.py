d48b69d6c1343cce58071683f04fff0f5289217f
4Ilyas4 <122739941+4Ilyas4@users.noreply.github.com>
2023-05-28 22:08:35 +0600

    Add files via upload

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import click
 import web3
 import requests
 from client_createbucket import *
+from makemessagefile import *
+from oPtions import *
 
 
 @click.group()
@@ -11,17 +13,15 @@ def bucket():
 
 @click.command()
 def cmd_createbucket():
-    bucketName = getBucketNameByUrl(ctx)#in go
-    primarySpAddrStr = ctx.String(primarySPFlag)#in go
-
-    if primarySpAddrStr == "" :
-		spInfo = client.ListStorageProviders(c, False)
-		primarySpAddrStr = spInfo[0].GetOperatorAddress()
- 
-    opts = sdktypes.CreateBucketOptions{} #in go         make opts list with visivility   payment address etc
-	if paymentAddrStr != "":
-		opts.PaymentAddress = paymentAddrStr
-                
+    bucketName = "MyBucket"#getBucketNameByUrl(ctx)#in go
+    primarySpAddrStr = "https://gnfd-testnet-sp-1.bnbchain.org"#ctx.String(primarySPFlag)#in go
+    paymentAddrStr = "212121212"#хз\ Аманжол ты знаешь
+    opts = NewOpts(Visibility,TxOpts,PaymentAddrStr,ChargedQuota,IsAsyncMode)#sdktypes.CreateBucketOptions{} #in go ,make opts list with visivility ,payment ,address etc          
+    if paymentAddrStr != "":
+        opts.PaymentAddress = paymentAddrStr
+    #if primarySpAddrStr == "" : 
+	    #spInfo = 1#client.ListStorageProviders(c, False)
+	    #primarySpAddrStr = 1#spInfo[0].GetOperatorAddress()         
     createbucket(bucketName, primarySpAddrStr, opts)
 
 
