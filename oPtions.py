51d0e183ef0f55197bd07bc9fa8fe82a4db9eb46
4Ilyas4 <122739941+4Ilyas4@users.noreply.github.com>
2023-05-28 22:10:42 +0600

    Add files via upload

---
+++ oPtions.py
@@ -0,0 +1,14 @@
+class NewOpts:
+    def __init__(self,_Visibility,Tx_Opts,_PaymentAddress,_ChargedQuota,_IsAsyncMode):
+        self.Visibility = _Visibility
+        self.TxOpts = Tx_Opts
+        self.PaymentAddress = _PaymentAddress
+        self.ChargedQuota = _ChargedQuota
+        self.IsAsyncMode = _IsAsyncMode
+
+   
+#storageTypes.VisibilityType
+#*gnfdsdktypes.TxOption
+#string
+ #uint64
+ #bool 
